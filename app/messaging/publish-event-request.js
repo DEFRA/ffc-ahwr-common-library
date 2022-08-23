168a191e253dfc13d53f6cca16617df34f5c449c
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-08-23 11:49:50 +0100

    Create a reusable event publisher npm package `ffc-awhr-event-publisher`
    * creation of an event publisher npm package

--- app/messaging/publish-event-request.js
+++ app/messaging/publish-event-request.js
@@ -5,7 +5,7 @@ const publishEventRequest = async (eventMessage, config, type) => {
   const eventSender = new MessageSender(config)
   const messageType = eventMessage.properties.action.type
   const source = eventMessage.properties.checkpoint
-  eventMessage.properties.action.timestamp = new Date().toISOString()
+  eventMessage.properties.action.raisedOn = new Date().toISOString()
   const message = createMessage(eventMessage, messageType, source)
   await eventSender.sendMessage(message)
 }
