e60fa4dae266527d98c0223fbfd5e819e64fc85a
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-03-19 09:18:46 +0000

    remove default export and package.json exports string

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { PublishEvent } from "./app/event-publisher/publish-event.js";
 import { PublishEventBatch } from "./app/event-publisher/publish-event-batch.js";
 
-export default {
+export {
   PublishEvent,
   PublishEventBatch,
 };
