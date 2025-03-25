2ea7b0d60cb25dde7c5985143a67fa944c5a138b
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-03-25 17:27:14 +0000

    feat(AHWR-262): added claim status constant

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { PublishEvent } from "./app/event-publisher/publish-event.js";
 import { PublishEventBatch } from "./app/event-publisher/publish-event-batch.js";
+import { CLAIM_STATUS } from "./app/constants.js";
 
 export {
   PublishEvent,
   PublishEventBatch,
+  CLAIM_STATUS,
 };
