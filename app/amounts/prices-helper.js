0b37f0a7c8ff686113433ac0926094aa090265f2
Aaron Carroll <20041831+aaroncarroll82@users.noreply.github.com>
2025-10-24 10:03:12 +0100

    Add new constants. Remove deprecated constants (#38)
    * Add new constants. Remove deprecated constants

---
+++ app/amounts/prices-helper.js
@@ -0,0 +1,84 @@
+import {
+  basicTestResultStatus,
+  claimType as CLAIM_TYPE,
+  PI_HUNT_AND_DAIRY_FOLLOW_UP_RELEASE_DATE,
+  TYPE_OF_LIVESTOCK,
+} from "../constants.js";
+import { prices } from "./prices.js";
+
+const isVisitDateAfterPIHuntAndDairyGoLive = (dateOfVisit) => {
+  const dateOfVisitParsed = new Date(dateOfVisit);
+  if (Number.isNaN(dateOfVisitParsed.getTime())) {
+    throw new TypeError(
+      `dateOfVisit must be parsable as a date, value provided: ${dateOfVisit}`
+    );
+  }
+
+  return dateOfVisitParsed >= PI_HUNT_AND_DAIRY_FOLLOW_UP_RELEASE_DATE;
+};
+
+const getPiHuntValue = (
+  reviewTestResults,
+  piHunt,
+  piHuntAllAnimals,
+  claimType,
+  typeOfLivestock
+) => {
+  const optionalPiHuntValue =
+    piHunt === "yes" && piHuntAllAnimals === "yes" ? "yesPiHunt" : "noPiHunt";
+
+  if (reviewTestResults === basicTestResultStatus.positive) {
+    return prices[claimType][typeOfLivestock].value[reviewTestResults];
+  }
+
+  return prices[claimType][typeOfLivestock].value[reviewTestResults][
+    optionalPiHuntValue
+  ];
+};
+
+const getNonPiHuntValue = (reviewTestResults, claimType, typeOfLivestock) => {
+  if (reviewTestResults === basicTestResultStatus.positive) {
+    return prices[claimType][typeOfLivestock].value[reviewTestResults];
+  }
+
+  return prices[claimType][typeOfLivestock].value[reviewTestResults].noPiHunt;
+};
+
+const getBeefDairyAmount = (data, claimType) => {
+  const {
+    typeOfLivestock,
+    reviewTestResults,
+    piHunt,
+    piHuntAllAnimals,
+    dateOfVisit,
+  } = data;
+
+  if (isVisitDateAfterPIHuntAndDairyGoLive(dateOfVisit)) {
+    return getPiHuntValue(
+      reviewTestResults,
+      piHunt,
+      piHuntAllAnimals,
+      claimType,
+      typeOfLivestock
+    );
+  }
+
+  return getNonPiHuntValue(reviewTestResults, claimType, typeOfLivestock);
+};
+
+export const getAmount = (data) => {
+  const { type, typeOfLivestock, reviewTestResults } = data;
+  const typeOfClaim = type === CLAIM_TYPE.review ? "review" : "followUp";
+
+  if (
+    [TYPE_OF_LIVESTOCK.BEEF, TYPE_OF_LIVESTOCK.DAIRY].includes(
+      typeOfLivestock
+    ) &&
+    reviewTestResults &&
+    type === CLAIM_TYPE.endemics
+  ) {
+    return getBeefDairyAmount(data, typeOfClaim);
+  }
+
+  return prices[typeOfClaim][typeOfLivestock].value;
+};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { PublishEvent } from "./app/event-publisher/publish-event.js";
 export { PublishEventBatch } from "./app/event-publisher/publish-event-batch.js";
 export * from "./app/constants.js";
-export * from "./app/prices.js";
+export * from "./app/amounts/prices.js";
+export * from "./app/amounts/prices-helper.js";
