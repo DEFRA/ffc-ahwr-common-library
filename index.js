eedf57ce1b714b50e1a2d6cf9e0a54752b29a803
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-11-13 09:32:43 +0000

    fix(AHWR-1366): fix exports for fcp-messaging

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-export { PublishEvent } from "./app/event-publisher/publish-event.js";
-export { PublishEventBatch } from "./app/event-publisher/publish-event-batch.js";
+export * from "./app/fcp-messaging/create-event-message.js";
+export * from "./app/fcp-messaging/create-message.js";
+export * from "./app/fcp-messaging/event-publisher.js";
+export * from "./app/fcp-messaging/event-schema.js";
+export * from "./app/fcp-messaging/service-bus-client.js";
 export * from "./app/constants.js";
 export * from "./app/amounts/prices.js";
 export * from "./app/amounts/prices-helper.js";
