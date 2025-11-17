2926ee5f313f3cec0d144fc237e73ed298af9b7c
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-11-17 14:16:02 +0000

    fix(AHWR-1366): convert status to status id for event messages

--- app/fcp-messaging/create-event-message.js
+++ app/fcp-messaging/create-event-message.js
@@ -1,4 +1,31 @@
 import { createMessage } from "./create-message.js";
+import { STATUS } from "../constants.js";
+
+const STATUS_ID_BY_STATUS = {
+  [STATUS.AGREED]: 1,
+  [STATUS.WITHDRAWN]: 2,
+  [STATUS.IN_CHECK]: 5,
+  [STATUS.ACCEPTED]: 6,
+  [STATUS.NOT_AGREED]: 7,
+  [STATUS.PAID]: 8,
+  [STATUS.READY_TO_PAY]: 9,
+  [STATUS.REJECTED]: 10,
+  [STATUS.ON_HOLD]: 11,
+  [STATUS.RECOMMENDED_TO_PAY]: 12,
+  [STATUS.RECOMMENDED_TO_REJECT]: 13,
+  [STATUS.AUTHORISED]: 14,
+  [STATUS.SENT_TO_FINANCE]: 15,
+  [STATUS.PAYMENT_HELD]: 16,
+}
+
+const replaceStatusId = (type) => {
+  const status = type.split(':').pop();
+  const statusId = STATUS_ID_BY_STATUS[status];
+
+  if (!statusId) return type;
+
+  return type.replace(status, statusId);
+}
 
 export const createEventMessage = ({
   name,
@@ -13,6 +40,7 @@ export const createEventMessage = ({
   raisedBy,
   raisedOn = new Date().toISOString(),
 }) => {
+  const normalisedType = replaceStatusId(type)
   const event = {
     name,
     properties: {
@@ -22,7 +50,7 @@ export const createEventMessage = ({
       checkpoint,
       status,
       action: {
-        type,
+        type: normalisedType,
         message,
         data,
         raisedBy,
@@ -31,5 +59,5 @@ export const createEventMessage = ({
     },
   };
 
-  return createMessage(event, type, checkpoint);
+  return createMessage(event, normalisedType, checkpoint);
 };
