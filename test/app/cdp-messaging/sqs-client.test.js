dd7c37b45b33cc19d5e9086c665d7467511334c2
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2026-03-17 15:35:58 +0000

    formatting

--- test/app/cdp-messaging/sqs-client.test.js
+++ test/app/cdp-messaging/sqs-client.test.js
@@ -85,45 +85,45 @@ describe("peekMessages", () => {
       .mockResolvedValueOnce({ Messages: [duplicateMessage] })
       .mockResolvedValueOnce({ Messages: [duplicateMessage] })
       .mockResolvedValue({});
-  
+
     setupClient("eu-west-1", "http://localhost:4566", logger);
     const result = await peekMessages("test-queue", 5, {});
-  
+
     expect(result).toHaveLength(1);
     expect(result[0].id).toBe("1");
   });
-  
+
   it("stops polling after 3 consecutive empty responses", async () => {
     sendMock.mockResolvedValue({ Messages: [] });
-  
+
     setupClient("eu-west-1", "http://localhost:4566", logger);
     const result = await peekMessages("test-queue", 10, {});
-  
+
     expect(sendMock).toHaveBeenCalledTimes(3);
     expect(result).toEqual([]);
   });
-  
+
   it("stops polling once limit is reached", async () => {
     sendMock
       .mockResolvedValueOnce({ Messages: [createMsg("1"), createMsg("2")] })
       .mockResolvedValueOnce({ Messages: [createMsg("3")] });
-  
+
     setupClient("eu-west-1", "http://localhost:4566", logger);
     const result = await peekMessages("test-queue", 3, {});
-  
+
     expect(result).toHaveLength(3);
     expect(sendMock).toHaveBeenCalledTimes(2);
   });
-  
+
   it("accumulates messages across multiple polls", async () => {
     sendMock
       .mockResolvedValueOnce({ Messages: [createMsg("1")] })
       .mockResolvedValueOnce({ Messages: [createMsg("2")] })
       .mockResolvedValue({});
-  
+
     setupClient("eu-west-1", "http://localhost:4566", logger);
     const result = await peekMessages("test-queue", 5, {});
-  
+
     expect(result).toHaveLength(2);
     expect(result.map((m) => m.id)).toEqual(["1", "2"]);
   });
